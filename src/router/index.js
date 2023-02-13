c23d6f99cced841632169d71152cda8124c6fcb9
TANG CHENG LEE <virgil246@gmail.com>
2023-02-14 05:09:56 +0800

    change  createWebHistory param

--- src/router/index.js
+++ src/router/index.js
@@ -34,9 +34,9 @@ const routes = [
   },
 ];
 
+
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 });
-
 export default router;
