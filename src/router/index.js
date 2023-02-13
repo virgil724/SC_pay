cfa35e30923c2f3becfa642a3790a5abf3b95c0d
TANG CHENG LEE <virgil246@gmail.com>
2023-02-14 05:13:49 +0800

    change to hashrouter

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 // Composables
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 
 const routes = [
   {
@@ -36,7 +36,7 @@ const routes = [
 
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes,
 });
 export default router;
