21de8117b90c607868b590b51c8f638aa8285d07
TANG CHENG LEE <virgil246@gmail.com>
2023-02-14 04:23:33 +0800

    change vite base

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"./SC_pay",
   plugins: [
     vue({ 
       template: { transformAssetUrls }
@@ -38,4 +39,5 @@ export default defineConfig({
   server: {
     port: 3000,
   },
+  
 })
